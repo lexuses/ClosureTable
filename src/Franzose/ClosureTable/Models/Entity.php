ab1ca342d9a175c0fc087b2bc2b116d4679a1659
Aleks Khrulev <lexuses@gmail.com>
2017-09-29 13:11:06 +0300

    Fix wrong order in getAncestors function

--- src/Franzose/ClosureTable/Models/Entity.php
+++ src/Franzose/ClosureTable/Models/Entity.php
@@ -336,14 +336,14 @@ class Entity extends Eloquent implements EntityInterface
         switch ($column) {
             case 'ancestor':
                 $query = $this->join($closure, $ancestor, '=', $primary)
-                    ->orderBy('depth', 'desc')
+                    ->orderBy($closure.'.depth', 'desc')
                     ->orderBy('position')
                     ->where($descendant, '=', $this->getKey());
                 break;
 
             case 'descendant':
                 $query = $this->join($closure, $descendant, '=', $primary)
-                    ->orderBy('depth', 'desc')
+                    ->orderBy($closure.'.depth', 'desc')
                     ->orderBy('position')
                     ->where($ancestor, '=', $this->getKey());
                 break;
